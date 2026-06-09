b26169519ed4bc10219e313568af02aefef35b98
karel rehor <karl.koerner@bonitoo.io>
2026-06-09 14:56:09 +0200

    test: revert envar check

--- scripts/on-release.sh
+++ scripts/on-release.sh
@@ -5,7 +5,7 @@ DATE=$(date)
 echo RELEASE WOULD BE STARTED HERE on $DATE
 echo From $0 SUPER_SECRET=\"$SUPER_SECRET\"
 
-if [ "$SUPER_SECRET" == "FooBar" ]; then
+if [ "$SUPER_SECRET" == "Wumpus!" ]; then
   echo SUPER_SECRET OK!
 else
   echo SUPER_SECRET Failed....
