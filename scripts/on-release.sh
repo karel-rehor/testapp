3babad910e6e505125429e2af8297ce98abcef65
karel rehor <karl.koerner@bonitoo.io>
2026-06-09 14:34:26 +0200

    test: leverage envars

--- scripts/on-release.sh
+++ scripts/on-release.sh
@@ -4,3 +4,10 @@ DATE=$(date)
 
 echo RELEASE WOULD BE STARTED HERE on $DATE
 echo From $0 SUPER_SECRET=\"$SUPER_SECRET\"
+
+if [ "$SUPER_SECRET" == "Wumpus!" ]; then
+  echo SUPER_SECRET OK!
+else
+  echo SUPER_SECRET Failed....
+  exit 1
+fi
